348319f2a55e6eca72bd2390f9904f41e9758574
shahab-cell <shahabuddin60@outlook.com>
2023-05-16 21:17:58 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
+// import Article from './components/article/Article'
+import { Brand, CTA, Navbar } from './components'
+import {
+  Blog,
+  Features,
+  Possibility,
+  WhatGPT3,
+  Header,
+  Footer,
+} from './containers'
+
+import './app.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCfrgjbln2dWfMBcSj82ELFUCt6wKIujoc',
+  authDomain: 'gpt3-fictional.firebaseapp.com',
+  projectId: 'gpt3-fictional',
+  storageBucket: 'gpt3-fictional.appspot.com',
+  messagingSenderId: '801075546759',
+  appId: '1:801075546759:web:2f980de918c29aa700c622',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
