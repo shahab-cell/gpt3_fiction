09b5b2b166ed7900d5fe9751d4905c6e1163f9a3
shahab-cell <shahabuddin60@outlook.com>
2023-05-17 15:50:57 +0500

    deploying on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyCfrgjbln2dWfMBcSj82ELFUCt6wKIujoc',
   authDomain: 'gpt3-fictional.firebaseapp.com',
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: '801075546759',
   appId: '1:801075546759:web:2f980de918c29aa700c622',
 }
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
